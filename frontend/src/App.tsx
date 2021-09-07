1d5eff5512326fd7a0ba41f42c07867b16e5d02b
DiegoCardosoDev <diegocardososacramento@gmail.com>
2021-09-07 12:31:15 -0300

    project-created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import NavBar from "components/NavBar";
+import Footer from "components/NavBar/Footer";
+
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+  
+    <div className="container">
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+
+      <div className="row px-3">
+      <div className="col-sm-6">
+        <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
+        <BarChart/>
+        </div>
+        <div className="col-sm-6">
+        <h5 className="text-center text-secundary">Todas Vendas</h5>
+        <DonutChart/>
+        </div>
+        </div>
+
+
+
+
+     
+    
+
+      <div className="py-3">
+        <h2 className="text-primary">Todas Vendas</h2>
+
+      </div>
+
+      <DataTable />
+      </div>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
