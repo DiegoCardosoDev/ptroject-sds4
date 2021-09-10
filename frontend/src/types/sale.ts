2f82d21c00b81332671f37591fe04cc6244b6020
DiegoCardosoDev <diegocardososacramento@gmail.com>
2021-09-10 14:47:56 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,13 @@
 import { type } from "os";
 
 export type SaleSum =   {
-    "sellerName": string,
-    "sum": number
+    "sellerName": string;
+    "sum": number;
 }
+
+export type saleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
+
